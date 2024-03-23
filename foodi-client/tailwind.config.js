dbf99a1333996c3a3bdfc65b5f02a9737829824e
unknown <rygailkim@gmail.com>
2024-03-23 15:18:53 +0800

    project setup

--- foodi-client/tailwind.config.js
+++ foodi-client/tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "green": "#39DB4A",
+        "red": "#FF6868",
+        "secondary": "#555",
+        "prigmayBG": "#FCFCFC",
+      }
+    },
   },
   plugins: [],
 }
