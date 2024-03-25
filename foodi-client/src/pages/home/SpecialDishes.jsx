9b6d454b0be995b0998f02b32dc3b29306f594ef
unknown <rygailkim@gmail.com>
2024-03-25 15:50:51 +0800

    banner and categories

---
+++ foodi-client/src/pages/home/SpecialDishes.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const SpecialDishes = () => {
+  return (
+    <div className='section-container  my-20'>
+        <div className='text-left'>
+            <p className='subtitle'>Special Dishes</p>
+            <h2 className='title md:w-[520px]'>Standout Dishes from our Menu</h2>
+        </div>
+
+    </div>
+  )
+}
+
+export default SpecialDishes
