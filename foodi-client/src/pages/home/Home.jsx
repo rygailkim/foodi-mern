770f641481a33cd101bd1f9f80404b3bf3e8b1a1
unknown <rygailkim@gmail.com>
2024-03-27 16:06:45 +0800

    part 1 done

--- foodi-client/src/pages/home/Home.jsx
+++ foodi-client/src/pages/home/Home.jsx
@@ -2,13 +2,18 @@ import React from 'react'
 import Banner from '../../components/Banner'
 import Categories from './Categories'
 import SpecialDishes from './SpecialDishes'
+import Testimonials from './Testimonials'
+import Service from './Service'
+import Footer from '../../components/Footer'
 
 const Home = () => {
   return (
-    <div className>
+    <div>
       <Banner/>
       <Categories/>
       <SpecialDishes/>
+      <Testimonials/>
+      <Service/>
     </div>
   )
 }
